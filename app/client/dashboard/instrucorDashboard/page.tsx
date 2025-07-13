5d1bba255116e5c91e86eb06060adb17ebfb8dfb
macbook <macbook@MacBook-Air.local>
2025-07-13 15:00:20 +0100

    Fixed errors

--- app/client/dashboard/instrucorDashboard/page.tsx
+++ app/client/dashboard/instrucorDashboard/page.tsx
@@ -4,14 +4,6 @@ const page = () => {
   return (
     <div>page</div>
   )
-
-  const page = () => {
-    return (
-      <div>page</div>
-    )
-  }
-  
-  export default page
 }
 
 export default page
