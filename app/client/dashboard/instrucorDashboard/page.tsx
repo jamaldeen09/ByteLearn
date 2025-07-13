ff11328169d3eb7286a59384a2f7947bd462e55b
macbook <macbook@MacBook-Air.local>
2025-07-13 14:59:57 +0100

    Made more changes

---
+++ app/client/dashboard/instrucorDashboard/page.tsx
@@ -0,0 +1,17 @@
+
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+
+  const page = () => {
+    return (
+      <div>page</div>
+    )
+  }
+  
+  export default page
+}
+
+export default page
